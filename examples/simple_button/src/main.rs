42c74741dedd1139c99f2ea556bce4bf1e1ba43f
Willi Kappler <willi.kappler@uni-tuebingen.de>
2017-09-07 15:37:57 +0200

    first draw method for pushbutton

--- examples/simple_button/src/main.rs
+++ examples/simple_button/src/main.rs
@@ -44,7 +44,7 @@ quick_main!(|| -> Result<()> {
     let mut main_window = RuneWindowBuilder::new("Rune button window", 640, 480).on_close_quit().build();
 
     let button1 = PushButtonBuilder::new("Click me", 10, 10).set_event_handler(Button1{ counter: 0 }).build();
-    let button2 = PushButtonBuilder::new("Quit", 10, 10).set_event_handler(Button2{}).build();
+    let button2 = PushButtonBuilder::new("Quit", 10, 40).set_event_handler(Button2{}).build();
 
     main_window.add_widget(button1);
     main_window.add_widget(button2);
--- src/push_button.rs
+++ src/push_button.rs
@@ -1,3 +1,5 @@
+use sdl2::pixels;
+use sdl2::rect::Rect;
 
 use rune::{RuneAction, RuneMouseButton};
 use widget::{RuneWidget, BaseWidget};
@@ -20,61 +22,67 @@ pub struct PushButton {
     pressed: bool,
     event_handler: Box<PushButtonHandler>,
 }
- impl RuneWidget for PushButton {
-     fn on_mouse_press(&mut self, mouse_button: RuneMouseButton, x: i32, y: i32) -> Option<RuneAction> {
-         None
-     }
 
-     fn on_mouse_release(&mut self, mouse_button: RuneMouseButton, x: i32, y: i32) -> Option<RuneAction> {
-         None
-     }
+impl RuneWidget for PushButton {
+    fn on_mouse_press(&mut self, mouse_button: RuneMouseButton, x: i32, y: i32) -> Option<RuneAction> {
+        None
+    }
 
-     fn draw(&mut self, canvas: &mut RuneCanvas) {
-     }
+    fn on_mouse_release(&mut self, mouse_button: RuneMouseButton, x: i32, y: i32) -> Option<RuneAction> {
+        None
+    }
+
+    fn draw(&mut self, canvas: &mut RuneCanvas) {
+        canvas.sdl_canvas.set_draw_color(pixels::Color::RGB(255, 255, 255));
+        canvas.sdl_canvas.draw_rect(Rect::new(
+             self.base_widget.x as i32, self.base_widget.y as i32,
+             self.base_widget.width, self.base_widget.height));
+        canvas.sdl_canvas.present();
+    }
+}
+
+pub struct PushButtonBuilder {
+    x: u32,
+    y: u32,
+    width: u32,
+    height: u32,
+    text: String,
+    event_handler: Box<PushButtonHandler>,
  }
 
- pub struct PushButtonBuilder {
-     x: u32,
-     y: u32,
-     width: u32,
-     height: u32,
-     text: String,
-     event_handler: Box<PushButtonHandler>,
- }
 
 
+impl PushButtonBuilder {
+    pub fn new(text: &str, x: u32, y: u32) -> PushButtonBuilder {
+        PushButtonBuilder {
+            x,
+            y,
+            width: 50,
+            height: 10,
+            text: text.to_string(),
+            event_handler: Box::new(DefaultHandler{}),
+        }
+    }
 
- impl PushButtonBuilder {
-     pub fn new(text: &str, x: u32, y: u32) -> PushButtonBuilder {
-         PushButtonBuilder {
-             x,
-             y,
-             width: 0,
-             height: 0,
-             text: text.to_string(),
-             event_handler: Box::new(DefaultHandler{}),
-         }
-     }
+    pub fn set_event_handler<T>(self, event_handler: T) -> PushButtonBuilder where T: 'static + PushButtonHandler {
+        PushButtonBuilder {
+            event_handler: Box::new(event_handler),
+            .. self
+        }
+    }
 
-     pub fn set_event_handler<T>(self, event_handler: T) -> PushButtonBuilder where T: 'static + PushButtonHandler {
-         PushButtonBuilder {
-             event_handler: Box::new(event_handler),
-             .. self
-         }
-     }
-
-     pub fn build(self) -> PushButton {
-         PushButton {
-             base_widget: BaseWidget {
-                 id: 0,
-                 x: self.x,
-                 y: self.y,
-                 width: self.width,
-                 height: self.height,
-             },
-             text: self.text,
-             pressed: false,
-             event_handler: self.event_handler,
-         }
-     }
- }
+    pub fn build(self) -> PushButton {
+        PushButton {
+            base_widget: BaseWidget {
+                id: 0,
+                x: self.x,
+                y: self.y,
+                width: self.width,
+                height: self.height,
+            },
+            text: self.text,
+            pressed: false,
+            event_handler: self.event_handler,
+        }
+    }
+}
