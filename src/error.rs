a8430320da18329ed73536b12e86e8fe8a503041
Willi Kappler <willi.kappler@uni-tuebingen.de>
2018-08-10 12:41:28 +0200

    clean branch

--- src/error.rs
+++
@@ -1,9 +0,0 @@
-use sdl2;
-
-error_chain! {
-    foreign_links {
-        WindowError(sdl2::video::WindowBuildError);
-        SDLError(sdl2::IntegerOrSdlError);
-        SDLTTFError(sdl2::ttf::InitError);
-    }
-}
